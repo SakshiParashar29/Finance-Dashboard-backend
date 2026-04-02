a7eb0bf843b0a286f026b16f3321deb323cdd980
Sakshi <sakshiparashar2910@gmail.com>
2026-04-02 15:06:22 +0530

    feat(routes): add auth routes and verify signup and login

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const {connectDB} = require('./config/db')
+const authRouter = require('./routes/auth-routes');
+
 
 const app = express();
 connectDB();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
