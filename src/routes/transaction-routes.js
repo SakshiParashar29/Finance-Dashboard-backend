d55ba351a8e0c7b1802a503c042c30de19ad9390
Sakshi <sakshiparashar2910@gmail.com>
2026-04-03 10:41:02 +0530

    fix: change updateUserRole and updateTransaction route to PATCH

--- src/routes/transaction-routes.js
+++ src/routes/transaction-routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/create', authMiddleware, roleMiddleware('Admin'), createTransaction);
 router.get('/all', authMiddleware, roleMiddleware('Analyst', 'Admin'), getAllTransaction);
 router.get('/:id', authMiddleware, roleMiddleware("Analyst", 'Admin'), getTransactionById);
-router.put('/:id', authMiddleware, roleMiddleware('Admin'), updateTransaction);
+router.patch('/:id', authMiddleware, roleMiddleware('Admin'), updateTransaction);
 router.delete('/:id', authMiddleware, roleMiddleware('Admin'), deleteTransaction);
 
 module.exports = router;
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/profile', authMiddleware, getUserProfile);
 router.get('/all', authMiddleware, roleMiddleware('Admin'), getAllUsers);
 router.get('/:id', authMiddleware, roleMiddleware('Admin'), getUserById)
-router.post('/role/:id', authMiddleware, roleMiddleware('Admin'), updateUserRole);
+router.patch('/role/:id', authMiddleware, roleMiddleware('Admin'), updateUserRole);
 router.patch('/activate/:id', authMiddleware, roleMiddleware("Admin"), activateUser);
 router.patch('/deactivate/:id', authMiddleware, roleMiddleware("Admin"), deactivateUser);
 
